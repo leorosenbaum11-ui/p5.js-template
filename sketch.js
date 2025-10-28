73d06f87e31abc52338a2ccea7036484a08e6f77
leo.rsb <leorosenbaum11@gmail.com>
2025-10-28 19:32:56 +0000

    improved harmonic motion

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
 let theta
+let theta2
+let xPos
 
 function setup() {
   createCanvas(400, 400);
   theta=0
+  theta2 = PI/2
+  colorMode(HSB)
+  background(0);
+  xPos = 0
 }
 
 function draw() {
-  background(220);
+  let hue = map(sin(theta2), -1, 1, 0, 360)
+  noFill()
+  stroke(hue, 100, 100)
   let basicSize = sin(theta)
   let realSize = map(basicSize, -1, 1, 10, 200)
-  circle(width/2, height/2, realSize)
-  theta+=0.01
+  circle(xPos, height/2, realSize)
+  theta+=200
+  theta2+=0.01
+  xPos += 1
 }
