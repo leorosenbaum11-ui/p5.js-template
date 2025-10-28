374859a44c2c99a129072fbe8bcc78f028d7e1f6
leo.rsb <leorosenbaum11@gmail.com>
2025-10-28 19:33:23 +0000

    idk mama

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function draw() {
   let basicSize = sin(theta)
   let realSize = map(basicSize, -1, 1, 10, 200)
   circle(xPos, height/2, realSize)
-  theta+=200
+  theta+=0.01
   theta2+=0.01
   xPos += 1
 }
